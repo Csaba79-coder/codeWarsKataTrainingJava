85ec6cb41bb44e164a1a45b1c2dd1dc5e6fc057f
Csaba79-coder <csabavadasz79@gmail.com>
2022-07-08 22:08:33 +0200

    solving 6 kyu kata's challenges

---
+++ src/main/java/Kyu6/HighestScoringWord.java
@@ -0,0 +1,95 @@
+package Kyu6;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class HighestScoringWord {
+
+    // https://www.codewars.com/kata/57eb8fcdf670e99d9b000272/train/java
+
+    public static String high(String s) {
+        // Your code here...
+        String highScoreWord = "";
+        int maxTotal = 0;
+        for (String word : s.split(" ")) {
+            int totalOfWord = 0;
+            for (char character : word.toCharArray()) {
+                totalOfWord += character - 96; // the int value of "a" is 97
+            }
+            if (totalOfWord > maxTotal) {
+                maxTotal = totalOfWord;
+                highScoreWord = word;
+            }
+        }
+        return highScoreWord;
+    }
+
+    public static String high2(String s) {
+        return Arrays.stream(s.split(" "))
+                .max(Comparator.comparingInt(
+                        a -> a.chars().map(i -> i - 96).sum()
+                )).get();
+    }
+
+    public static String high3(String s) {
+
+        String winner = "";
+        int highScore = 0;
+
+        for (String word : s.split(" ")) {
+            int score = 0;
+            for (char character : word.toCharArray()) {
+                score += character - 'a' + 1;
+            }
+            if (score > highScore) {
+                winner = word;
+                highScore = score;
+            }
+        }
+
+        return winner;
+    }
+
+    public static String high4(String s) {
+        String[] arr = s.split(" ");
+        int[] sums = new int[arr.length];
+        for(int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for(int j = 0; j < arr[i].length(); j++) {
+                sum += arr[i].charAt(j) - 96;
+                sums[i] = sum;
+            }
+        }
+
+        int max = sums[0];
+        int maxIndex = 0;
+
+        for(int i = 0; i < sums.length; i++) {
+            if(sums[i] > max) {
+                max = sums[i];
+                maxIndex = i;
+            }
+        }
+        return arr[maxIndex];
+    }
+
+    public static String high5(String s) {
+        return Arrays.stream(s.split(" "))
+                .sorted(Comparator.comparingInt(HighestScoringWord::calculateWordScore).reversed())
+                .findFirst().get();
+    }
+
+    private static int calculateWordScore(String word) {
+        return word.chars().map(c -> c - (int)'a' + 1).sum();
+    }
+
+    public static String high6(String s) {
+        String word = "";
+        int n, max = 0;
+        for (String w : s.split(" ")) {
+            if ((n = w.chars().sum() - w.length() * ('a'-1)) > max) {
+                word = w; max = n; }
+        }
+        return word;
+    }
+}
--- src/main/java/Kyu6/SortTheOdd.java
+++ src/main/java/Kyu6/SortTheOdd.java
@@ -1,11 +1,14 @@
 package Kyu6;
 
+import java.util.*;
+import java.util.stream.IntStream;
+
 public class SortTheOdd {
 
     // https://www.codewars.com/kata/578aa45ee9fd15ff4600090d/train/java
 
     public static int[] sortArray(int[] array) {
-        int min = 0;
+        int min;
         for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 == 0) {
             } else {
@@ -20,6 +23,100 @@ public class SortTheOdd {
                     }
                 }
             }
+        }
+        return array;
+    }
+
+    public static int[] sortArray2(int[] array) {
+        PrimitiveIterator.OfInt sortedOdds = IntStream
+                .of(array)
+                .filter(i -> i % 2 == 1)
+                .sorted()
+                .iterator();
+
+        return IntStream
+                .of(array)
+                .map(i -> i % 2 == 0 ? i : sortedOdds.nextInt())
+                .toArray();
+    }
+
+    public static int[] sortArray3(int[] array) {
+        for(int i=0 ; i<array.length-1 ; i++){
+            for(int j=i+1; j<array.length ; j++){
+                if(array[i]>array[j] && array[i]%2 == 1 && array[j]%2 == 1){
+                    int aux=array[i];
+                    array[i]=array[j];
+                    array[j]=aux;
+                }
+            }
+        }
+        return array;
+    }
+
+    public static int[] sortArray4(int[] array) {
+        ArrayList<Integer> oddList = new ArrayList<>(array.length);
+        for (int k : array) {
+            if (k % 2 == 1) {
+                oddList.add(k);
+            }
+        }
+        Collections.sort(oddList);
+
+        for (int i = 0, j = 0; i < oddList.size(); i++, j++) {
+            while (array[j] % 2 == 0) {
+                j++;
+            }
+            array[j] = oddList.get(i);
+        }
+        return array;
+    }
+
+    public static int[] sortArray5(final int[] array) {
+        final Iterator<Integer> sortedOdds = IntStream.of(array)
+                .filter(SortTheOdd::isOdd)
+                .sorted()
+                .iterator();
+        return IntStream.of(array)
+                .map(n -> isOdd(n) ? sortedOdds.next() : n)
+                .toArray();
+    }
+
+    public static boolean isOdd(final int n) {
+        return n % 2 != 0;
+    }
+
+    public static int[] sortArray6(int[] array) {
+        if (array.length == 0)
+            return array;
+
+        // linear sort skipping even numbers.
+        for (int i=0; i<array.length; i++) {
+            if (array[i] % 2 != 0) {
+                for (int j=0; j<i; j++) {
+                    if (array[j] % 2 != 0 && array[j] > array[i]) {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+
+        return array;
+    }
+
+    static int[] sortArray7(final int[] array) {
+        List<Integer> odds = new ArrayList<>();
+        List<Integer> indices = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 1) {
+                indices.add(i);
+                odds.add(array[i]);
+            }
+        }
+        odds.sort(Integer::compareTo);
+        for (int i = 0; i < indices.size(); i++) {
+            array[indices.get(i)] = odds.get(i);
         }
         return array;
     }
